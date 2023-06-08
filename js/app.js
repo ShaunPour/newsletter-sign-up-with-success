52b18c74dd1c3a81b225b99fe2cf5dbb7c668bba
Shaun <scpour@gmail.com>
2023-06-08 16:23:03 -0400

    Enabled javascript to switch between default and successful display states

--- js/app.js
+++ js/app.js
@@ -4,15 +4,25 @@ const form = document.querySelector('form');
 const email = document.querySelector('input');
 const btn = document.querySelector('button');
 const success = document.querySelector('.success');
+const successBtn = document.querySelector('.success button');
 
 form.addEventListener('click', (e) => {
     e.preventDefault();
+});
 
-    btn.addEventListener('click', () => {
+btn.addEventListener('click', () => {
         if(email.value.includes('@')) {
             header.classList.toggle('hide');
             main.classList.toggle('hide');
             success.classList.toggle('success-show');
         }
-    })
-})
+    });
+
+    successBtn.addEventListener('click', () => {
+        if(email.value.includes('@')) {
+            header.classList.toggle('hide');
+            main.classList.toggle('hide');
+            success.classList.toggle('success-show');
+            email.value = '';
+        }
+    });
