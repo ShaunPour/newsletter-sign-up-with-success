85f7076ec09a91739a379745ca8767d0130720e6
Shaun <scpour@gmail.com>
2023-07-05 12:22:00 -0400

    Fixed display for the desktop viewport and reconfigured success screen for desktop

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
-const header = document.querySelector('header');
-const main = document.querySelector('main');
+const container = document.querySelector('.container');
+const mobile = document.querySelector('.mobile-img');
+const desktop = document.querySelector('.desktop-img');
+const main = document.querySelector('.main');
 const form = document.querySelector('form');
 const email = document.querySelector('input');
 const btn = document.querySelector('button');
@@ -13,20 +15,30 @@ form.addEventListener('click', (e) => {
 
 btn.addEventListener('click', () => {
         if(email.value.includes('@')) {
-            header.classList.toggle('hide');
+            container.classList.toggle('hide');
             main.classList.toggle('hide');
+            mobile.classList.toggle('hide');
+            desktop.style.display = 'none';
+            container.style.paddingBottom = '46vh';
+            container.style.width = '33.33%';
+            container.style.left = '30vw';
             success.classList.toggle('success-show');
             email.classList.remove('form-error');
-            error.classList.add('hide');
+            error.classList.toggle('hide');
         } else {
-            email.classList.add('form-error');
-            error.classList.remove('hide');
+            email.classList.toggle('form-error');
+            error.classList.toggle('hide');
         }
     });
 
     successBtn.addEventListener('click', () => {
         if(email.value.includes('@')) {
-            header.classList.toggle('hide');
+            container.classList.toggle('hide');
+            mobile.classList.toggle('hide');
+            desktop.style.display = 'initial';
+            container.style.paddingBottom = '0';
+            container.style.width = '75vw';
+            container.style.left = '8vw';
             main.classList.toggle('hide');
             success.classList.toggle('success-show');
             email.value = '';
