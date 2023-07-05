9be0446f7682ba39c76cb8e76ddc7213ddb8a44a
Shaun <scpour@gmail.com>
2023-07-05 08:53:14 -0400

    Reworked page structure to add elements to a card on the desktop viewport

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ const email = document.querySelector('input');
 const btn = document.querySelector('button');
 const success = document.querySelector('.success');
 const successBtn = document.querySelector('.success button');
+const error = document.querySelector('.error-msg');
 
 form.addEventListener('click', (e) => {
     e.preventDefault();
@@ -15,6 +16,11 @@ btn.addEventListener('click', () => {
             header.classList.toggle('hide');
             main.classList.toggle('hide');
             success.classList.toggle('success-show');
+            email.classList.remove('form-error');
+            error.classList.add('hide');
+        } else {
+            email.classList.add('form-error');
+            error.classList.remove('hide');
         }
     });
 
@@ -24,5 +30,7 @@ btn.addEventListener('click', () => {
             main.classList.toggle('hide');
             success.classList.toggle('success-show');
             email.value = '';
+            email.classList.remove('form-error');
+            error.classList.add('hide');
         }
     });
